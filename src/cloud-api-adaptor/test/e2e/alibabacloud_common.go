ccb79ae1bb5c152a940ba8e305d989e1a9bc01ee
Xynnn007 <xynnn@linux.alibaba.com>
2025-06-17 09:42:28 +0800

    e2e: add e2e test for alibaba cloud
    Alibaba cloud uses CLI tool to do image upload things and get instances
    information. Also, it uses an existing created cluster to do tests,
    which is named as "On-prem" cluster.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ src/cloud-api-adaptor/test/e2e/alibabacloud_common.go
@@ -0,0 +1,75 @@
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"testing"
+	"time"
+
+	ecs "github.com/alibabacloud-go/ecs-20140526/v4/client"
+	pv "github.com/confidential-containers/cloud-api-adaptor/src/cloud-api-adaptor/test/provisioner/alibabacloud"
+)
+
+// AlibabaCloudAssert implements the CloudAssert interface for alibaba cloud.
+type AlibabaCloudAssert struct{}
+
+func NewAlibabaCloudAssert() *AlibabaCloudAssert {
+	return &AlibabaCloudAssert{}
+}
+
+func (c AlibabaCloudAssert) DefaultTimeout() time.Duration {
+	return 2 * time.Minute
+}
+
+// findVM is a helper function to find VMs bytheir prefix name.
+func describeInstances(prefixName string) ([]*ecs.DescribeInstancesResponseBodyInstancesInstance, error) {
+	cmd := exec.Command(pv.Cli, "ecs", "DescribeInstances", "--RegionId", pv.AlibabaCloudProps.Region, "--InstanceName", "podvm-*")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get instances: %v", err)
+	}
+
+	var response ecs.DescribeInstancesResponseBody
+	err = json.Unmarshal(out, &response)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse DescribeInstancesResponse: %v", err)
+	}
+
+	if response.Instances != nil {
+		return response.Instances.Instance, nil
+	}
+
+	return make([]*ecs.DescribeInstancesResponseBodyInstancesInstance, 0), nil
+}
+
+func isAlibabaCloudVMExisted(prefixName string) (bool, error) {
+	instances, err := describeInstances(prefixName)
+	if err != nil {
+		return false, fmt.Errorf("failed to query instances: %v", err)
+	}
+
+	return len(instances) == 0, nil
+}
+
+func (c AlibabaCloudAssert) HasPodVM(t *testing.T, id string) {
+	podVmPrefix := "podvm-" + id
+	has, err := isAlibabaCloudVMExisted(podVmPrefix)
+	if err != nil {
+		t.Logf("Error happens when checking the VM: %v", err)
+	}
+	if has {
+		t.Logf("VM %s found", id)
+	} else {
+		t.Logf("Virtual machine %s not found ", id)
+		t.Error("PodVM was not created")
+	}
+}
+
+func (c AlibabaCloudAssert) GetInstanceType(t *testing.T, podName string) (string, error) {
+	// Get Instance Type of PodVM
+	return "", nil
+}
---
+++ src/cloud-api-adaptor/test/provisioner/alibabacloud/provision_common.go
@@ -0,0 +1,257 @@
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package alibabacloud
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	pv "github.com/confidential-containers/cloud-api-adaptor/src/cloud-api-adaptor/test/provisioner"
+	log "github.com/sirupsen/logrus"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+)
+
+const (
+	Cli                         = "aliyun"
+	AlibabaCloudCredentialsFile = "alibabacloud-cred.env"
+)
+
+var AlibabaCloudProps = &OnPremCluster{}
+
+// Cluster defines create/delete/access interfaces to Kubernetes clusters
+type Cluster interface {
+	CreateCluster() error               // Create the Kubernetes cluster
+	DeleteCluster() error               // Delete the Kubernetes cluster
+	GetKubeconfigFile() (string, error) // Get the path to the kubeconfig file
+}
+
+// OnPremCluster represents an existing and running cluster
+type OnPremCluster struct {
+	PodVMImageId    string
+	Region          string
+	RrsaRoleArn     string
+	RrsaProviderArn string
+	OssBucket       string
+	OssEndpoint     string
+	CaaImage        string
+}
+
+// NewAlibabaCloudProvisioner instantiates the AlibabaCloud provisioner
+// The AlibabaCloudProvisioner will use aliyun cli.
+func NewAlibabaCloudProvisioner(properties map[string]string) (pv.CloudProvisioner, error) {
+	_, err := exec.LookPath(Cli)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Alibaba Cloud CLI tool (aliyun): %v", err)
+	}
+
+	if properties["cluster_type"] == "" ||
+		properties["cluster_type"] == "onprem" {
+		AlibabaCloudProps = &OnPremCluster{
+			PodVMImageId:    properties["pod_vm_image_id"],
+			Region:          properties["region"],
+			RrsaRoleArn:     properties["rrsa_role_arn"],
+			RrsaProviderArn: properties["rrsa_provider_arn"],
+			OssBucket:       properties["oss_bucket"],
+			OssEndpoint:     properties["oss_endpoint"],
+			CaaImage:        properties["caa_image"],
+		}
+
+		return AlibabaCloudProps, nil
+	} else {
+		return nil, fmt.Errorf("Cluster type '%s' not implemented",
+			properties["cluster_type"])
+	}
+}
+
+// CreateCluster does nothing as the cluster should exist already.
+func (o *OnPremCluster) CreateCluster(ctx context.Context, cfg *envconf.Config) error {
+	log.Info("On-prem cluster type selected. Nothing to do.")
+
+	return nil
+}
+
+// DeleteCluster does nothing.
+func (o *OnPremCluster) DeleteCluster(ctx context.Context, cfg *envconf.Config) error {
+	log.Info("On-prem cluster type selected. Nothing to do.")
+
+	return nil
+}
+
+// CreateVPC does nothing.
+func (o *OnPremCluster) CreateVPC(ctx context.Context, cfg *envconf.Config) error {
+	log.Info("On-prem cluster type selected. Nothing to do.")
+
+	return nil
+}
+
+// DeleteVPC does nothing.
+func (a *OnPremCluster) DeleteVPC(ctx context.Context, cfg *envconf.Config) error {
+	log.Info("On-prem cluster type selected. Nothing to do.")
+	return nil
+}
+
+func (a *OnPremCluster) GetProperties(ctx context.Context, cfg *envconf.Config) map[string]string {
+	props := map[string]string{
+		"pod_vm_image_id":   a.PodVMImageId,
+		"region":            a.Region,
+		"rrsa_role_arn":     a.RrsaRoleArn,
+		"rrsa_provider_arn": a.RrsaProviderArn,
+		"oss_bucket":        a.OssBucket,
+		"oss_endpoint":      a.OssEndpoint,
+	}
+
+	return props
+}
+
+func (a *OnPremCluster) UploadPodvm(imagePath string, ctx context.Context, cfg *envconf.Config) error {
+	// Upload image to OSS
+	file, err := os.Open(imagePath)
+	if err != nil {
+		return fmt.Errorf("failed to open image file: %v", err)
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get image file stat: %v", err)
+	}
+	key := stat.Name()
+	cloudUrl := fmt.Sprintf("oss://%s/%s", a.OssBucket, key)
+	err = a.uploadOss(imagePath, cloudUrl)
+	if err != nil {
+		return err
+	}
+
+	// Import image as Pod VM image
+	err = a.importImage(key)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *OnPremCluster) uploadOss(filepath string, cloudUrl string) error {
+	cmd := exec.Command(Cli, "oss", "cp", filepath, cloudUrl, "-f")
+	out, err := cmd.CombinedOutput()
+	fmt.Printf("%s\n", out)
+	if err != nil {
+		return fmt.Errorf("failed to upload file to OSS: %v", err)
+	}
+
+	return nil
+}
+
+type ImportImageResponse struct {
+	RequestId string
+	ImageId   string
+	TaskId    string
+	RegionId  string
+}
+
+func (a *OnPremCluster) importImage(imageName string) error {
+	cmd := exec.Command(Cli, "ecs", "ImportImage", "--ImageName", imageName, "--region", a.Region, "--RegionId", a.Region, "--BootMode", "UEFI", "--DiskDeviceMapping.1.OSSBucket", a.OssBucket, "--DiskDeviceMapping.1.OSSObject", imageName, "--Features.NvmeSupport", "supported", "--method", "POST", "--force")
+	out, err := cmd.CombinedOutput()
+	fmt.Printf("%s\n", string(out))
+	if err != nil {
+		return fmt.Errorf("failed to import image: %v", err)
+	}
+
+	var r ImportImageResponse
+	err = json.Unmarshal(out, &r)
+	if err != nil {
+		return fmt.Errorf("failed to parse import image response: %v", err)
+	}
+
+	a.PodVMImageId = r.ImageId
+	return nil
+}
+
+type AlibabaCloudInstallOverlay struct {
+	Overlay *pv.KustomizeOverlay
+}
+
+func createRRSACredentialFile(dir, roleArn, providerArn string) error {
+	content := fmt.Sprintf("ALIBABA_CLOUD_ROLE_ARN=%s\nALIBABA_CLOUD_OIDC_PROVIDER_ARN=%s\nALIBABA_CLOUD_OIDC_TOKEN_FILE=/var/run/secrets/ack.alibabacloud.com/rrsa-tokens/token", roleArn, providerArn)
+	err := os.WriteFile(filepath.Join(dir, AlibabaCloudCredentialsFile), []byte(content), 0666)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func NewAlibabaCloudInstallOverlay(installDir, provider string) (pv.InstallOverlay, error) {
+	overlayDir := filepath.Join(installDir, "overlays", provider)
+
+	// The credential file should exist in the overlay directory otherwise kustomize fails
+	// to load it. At this point we don't know the key id nor access key, so using empty
+	// values (later the file will be re-written properly).
+	err := createRRSACredentialFile(overlayDir, AlibabaCloudProps.RrsaRoleArn, AlibabaCloudProps.RrsaProviderArn)
+	if err != nil {
+		return nil, err
+	}
+
+	overlay, err := pv.NewKustomizeOverlay(overlayDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AlibabaCloudInstallOverlay{
+		Overlay: overlay,
+	}, nil
+}
+
+func (a *AlibabaCloudInstallOverlay) Apply(ctx context.Context, cfg *envconf.Config) error {
+	return a.Overlay.Apply(ctx, cfg)
+}
+
+func (a *AlibabaCloudInstallOverlay) Delete(ctx context.Context, cfg *envconf.Config) error {
+	return a.Overlay.Delete(ctx, cfg)
+}
+
+func (a *AlibabaCloudInstallOverlay) Edit(ctx context.Context, cfg *envconf.Config, properties map[string]string) error {
+	var err error
+
+	image := strings.Split(properties["caa_image"], ":")[0]
+	tag := strings.Split(properties["caa_image"], ":")[1]
+	log.Infof("Updating caa image with %s", image)
+	if image != "" {
+		err = a.Overlay.SetKustomizeImage("cloud-api-adaptor", "newName", image)
+		if err != nil {
+			return fmt.Errorf("failed to set CAA image name: %v", err)
+		}
+	}
+	if tag != "" {
+		err = a.Overlay.SetKustomizeImage("cloud-api-adaptor", "newTag", tag)
+		if err != nil {
+			return fmt.Errorf("failed to set CAA image tag: %v", err)
+		}
+	}
+
+	// Mapping the internal properties to ConfigMapGenerator properties.
+	mapProps := map[string]string{
+		"pod_vm_image_id": "IMAGEID",
+		"region":          "REGION",
+	}
+
+	for k, v := range mapProps {
+		if properties[k] != "" {
+			if err = a.Overlay.SetKustomizeConfigMapGeneratorLiteral("peer-pods-cm",
+				v, properties[k]); err != nil {
+				return err
+			}
+		}
+	}
+
+	if err = a.Overlay.YamlReload(); err != nil {
+		return err
+	}
+
+	return nil
+}
