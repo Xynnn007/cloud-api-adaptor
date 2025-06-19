8648d3977b8686ec9cccb06d00002e385b2e6269
Xynnn007 <xynnn@linux.alibaba.com>
2025-06-19 13:48:42 +0800

    fixup! e2e: add e2e test for alibaba cloud

--- src/cloud-api-adaptor/test/provisioner/alibabacloud/provision_common.go
+++ src/cloud-api-adaptor/test/provisioner/alibabacloud/provision_common.go
@@ -11,6 +11,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	pv "github.com/confidential-containers/cloud-api-adaptor/src/cloud-api-adaptor/test/provisioner"
 	log "github.com/sirupsen/logrus"
@@ -122,14 +123,29 @@ func (a *OnPremCluster) UploadPodvm(imagePath string, ctx context.Context, cfg *
 		return fmt.Errorf("failed to get image file stat: %v", err)
 	}
 	key := stat.Name()
-	cloudUrl := fmt.Sprintf("oss://%s/%s", a.OssBucket, key)
+	// Execute the command `git rev-parse HEAD` to get git rev
+	cmd := exec.Command("git", "rev-parse", "HEAD")
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("failed to get current git rev: %v", err)
+	}
+
+	// Convert the output to string and trim any whitespace/newline characters
+	commitHash := strings.TrimSpace(string(output))
+
+	currentTime := time.Now()
+	timestampSec := currentTime.Unix()
+
+	imageName := fmt.Sprintf("%s-%s-%d", key, commitHash, timestampSec)
+
+	cloudUrl := fmt.Sprintf("oss://%s/%s", a.OssBucket, imageName)
 	err = a.uploadOss(imagePath, cloudUrl)
 	if err != nil {
 		return err
 	}
 
 	// Import image as Pod VM image
-	err = a.importImage(key)
+	err = a.importImage(imageName)
 	if err != nil {
 		return err
 	}
